a6f78cb27a8489ebdf1222df1650e856573c4243
ali-basit <alirashid_1995@hotmail.com>
2018-02-27 17:29:45 -0500

    build, fit, and make final evaluation of model

--- Handwritten-Digit-Recognition-Keras.py
+++ Handwritten-Digit-Recognition-Keras.py
@@ -40,3 +40,11 @@ def baseline_model():
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
+
+# build the model
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
